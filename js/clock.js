16a4296757f9c35943cac16bc8c534aff7539d34
resortcoder <mstfckmk70@yandex.com>
2022-10-19 08:44:20 +0300

    Saat, dakika, saniye başına sıfır eklendi.

--- js/clock.js
+++ js/clock.js
@@ -5,17 +5,16 @@ let isim = prompt("İsminiz nedir?");
 isimSpan.innerHTML = isim;
 
 function showTime() {
-    /* alert("TEST!");
-    console.log("showTime geldi!"); */
+
     let gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
 
     let date = new Date();
     let gunNumber = date.getDay();
-    let saat = date.getHours();
-    let dakika = date.getMinutes();
-    let saniye = date.getSeconds();
+    let saat = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    let dakika = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let saniye = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-    document.querySelector("#myClock").innerHTML = `${saat}:${dakika}:${saniye < 10 ? "0" + saniye : saniye} ${gunler[gunNumber]}`
+    document.querySelector("#myClock").innerHTML = `${saat}:${dakika}:${saniye} ${gunler[gunNumber]}`
 
 }
 setInterval(showTime, 1000);
